0812a605f96d49d3af8fff951ed19481c4b2e21d
Uli Rodriguez <fulizesraudales@gmail.com>
2023-05-30 21:57:51 -0700

    redoing things

--- Database/UserCredentials.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('Database/user_creds.yaml') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
-
-USER = data['username']
-USER_ID = data['user_id']
-
-INITIALIZED = data['initialized']
